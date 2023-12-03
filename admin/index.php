70e84e56131d79d562a37b50ed69c58a89b91746
Battai Istvan <krisztianadam766@gmail.com>
2023-12-03 23:26:53 +0100

    Code optimizing

--- admin/index.php
+++ admin/index.php
@@ -87,13 +87,8 @@ exit();
             </div>
         </div>
     </div>
-    <!-- CONTENT-WRAPPER SECTION END-->
     <?php include('includes/footer.php');?>
-    <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY SCRIPTS -->
     <script src="../assets/js/jquery-1.11.1.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
